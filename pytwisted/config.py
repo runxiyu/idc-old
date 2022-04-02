18b28c035d13f8724024eb5e5a286a9c2a5975cb
Andrew Yu <andrew@andrewyu.org>
2022-04-03 01:03:23 +0800

    memes

---
+++ pytwisted/config.py
@@ -0,0 +1,21 @@
+server_name = "home.andrewyu.org"
+users = {
+    "andrew": {
+        "password": "hunter2",
+        "bio": "Andrew Yu",
+        "permissions": {"god"},
+        "options": ["offline-messages", "eat-cookies"],
+    },
+    "hax": {
+        "password": "lurk",
+        "bio": "Professional h4xx0r",
+        "permissions": {"god"},
+        "options": ["offline-messages", "eat-cookies"],
+    },
+    "luk3yx": {
+        "password": "billy",
+        "bio": "Random bot",
+        "permissions": {"god"},
+        "options": ["offline-messages", "eat-cookies"],
+    },
+}
--- pytwisted/server.py
+++ pytwisted/server.py
@@ -5,25 +5,55 @@ clients to port 1025
 """
 
 from twisted.protocols import basic
+import random
+import config
 
 
 class MyChat(basic.LineReceiver):
     delimiter = b"\n"
     def connectionMade(self):
-        print("Got new client!")
-        self.factory.clients.append(self)
+        r = lambda: random.randchoice(list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+        while True:
+            _ = r() + r() + r() + r() + r() + r()
+            if _ not in self.factory.clients.keys():
+                self.cid = _
+                del _
+                del r
+                break
+        self.factory.clients[self.cid] = self
+        self.ready = False
+        self.username = None
 
     def connectionLost(self, reason):
-        print("Lost a client!")
-        self.factory.clients.remove(self)
+        del self.factory.clients[self.cid]
+        del self
 
     def lineReceived(self, line):
-        print("received", repr(line))
-        for c in self.factory.clients:
-            c.message(line)
+        print(repr(self.cid), repr(line))
+        escaped = False
+        args = []
+        current = b""
+        for b in line:
+            if escaped:
+                if b == b"\\":
+                    current += b"\\"
+                elif b == b"\t":
+                    current += b"\t"
+                else:
+                    current += b""
+                escaped = False
+            elif b == b"\\":
+                escaped = True
+            elif b == b"\t":
+                args.append(current)
+                current = b""
+            else:
+                current += b
+        self.send(bytes(repr(args)))
 
-    def message(self, message):
-        self.transport.write(message + b"\r\n")
+    def send(self, bytestring):
+        try:
+            self.transport.write(bytestring + b"\r\n")
 
 
 from twisted.internet import protocol
@@ -31,7 +61,8 @@ from twisted.application import service, internet
 
 factory = protocol.ServerFactory()
 factory.protocol = MyChat
-factory.clients = []
+factory.clients = {}
+factory.channels = {}
 
 application = service.Application("chatserver")
 internet.TCPServer(1025, factory).setServiceParent(application)
