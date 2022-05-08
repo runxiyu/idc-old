7a6bf8908c6699a3ff3ddb728966e46c3a7dd688
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-05-08 13:59:57 +0200

    SrIRCeBot: Licensed under AGPL
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

--- clients/srirce/main.py
+++ clients/srirce/main.py
@@ -1,4 +1,21 @@
-# srIRCeBot, a Simple IRC RElay Bot
+#    srIRCeBot, a Simple IRC RElay Bot
+#
+#    Copyright (C) 2022 Ferass EL HAFIDI <vitali64pmemail@protonmail.com>
+#    Copyright (C) 2022 Andrew Yu <andrew@andrewyu.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import miniirc
 import miniirc_idc
