b188c83c6c2ba96ff5795c12f112d89465d280fc
64bit dude <vitali64@andrewyu.org>
2022-05-08 13:52:07 +0200

    Fix SrIRCe
    Signed-off-by: 64bit dude <vitali64@andrewyu.org>

--- clients/srirce/main.py
+++ clients/srirce/main.py
@@ -1,5 +1,4 @@
 # srIRCeBot, a Simple IRC RElay Bot
-# you should license it or unlicense it or whatever
 
 import miniirc
 import miniirc_idc
@@ -8,173 +7,131 @@ from time import sleep
 
 # Configuration
 
-server = [["irc.andrewyu.org", "IRC"], ["andrewyu.org", "IDC"]]
-relayedChannels = ["#IDC", "#hackers"]
-nick = "idcbot"
-debug = True
+server          = [ ['irc.andrewyu.org', 'IRC'], ['andrewyu.org', 'IDC'] ]
+relayedChannels = [ '#IDC'                     , '#hackers'              ]
+nick            = 'idcbot'
+debug           = True
 
 # IRC
-irc = miniirc.IRC(
-    server[0][0],
-    6697,
-    nick,
-    relayedChannels[0],
-    ns_identity=None,
-    debug=debug,
-)
+irc = miniirc.IRC(server[0][0], 6697, nick, relayedChannels[0], ns_identity=None, debug=False)
 # IDC
-idc = miniirc_idc.IDC(
-    server[1][0],
-    6835,
-    nick,
-    relayedChannels[1],
-    ns_identity=["idcbot", ""],
-    debug=debug,
-)
+idc = miniirc_idc.IDC(server[1][0], 6835, nick, relayedChannels[1], ns_identity=["idcbot", ""], debug=debug)
 
 # Relay functions
 
-
 def relay_msgs(chat, hostmask, args, which):
-    sleep(0.5)  # Sleep for 0.5 seconds
+    #sleep(0.5) # Sleep for 0.5 seconds
     whichchan = 0 if which == 1 else 1
     # Send in the irc side what the user sent in the idc side and vice-versa
     print("NEW MESSAGE")
-    if chat is idc:
-        chat.idc_send(
-            "CHANMSG	TARGET=%s	MESSAGE=(%s) <%s> %s"
-            % (
-                relayedChannels[whichchan],
-                server[which][1],
-                hostmask[0],
-                args[-1],
-            )
-        )
-    else:
-        chat.send(
-            "PRIVMSG",
-            relayedChannels[whichchan],
-            "(%s) <%s> %s" % (server[which][1], hostmask[0], args[-1]),
-        )
-
+    chat.send('PRIVMSG', relayedChannels[whichchan], 
+                "(%s) <%s> %s" 
+                % (server[which][1], hostmask[0], args[-1]))
 
 def relay_nick(chat, hostmask, args, which):
-    sleep(0.5)
+    #sleep(0.5)
     whichchan = 0 if which == 1 else 1
     # Send in the idc side the nick change in the irc side
-    chat.send(
-        "PRIVMSG",
-        relayedChannels[whichchan],
-        "(%s) %s is now known as %s"
-        % (server[which][1], hostmask[0], args[0]),
-    )
-
+    chat.send('PRIVMSG', relayedChannels[whichchan], 
+                "(%s) %s is now known as %s" 
+                % (server[which][1], hostmask[0], args[0]))
 
 def relay_joins(chat, hostmask, args, which):
-    sleep(0.5)
+    #sleep(0.5)
     whichchan = 0 if which == 0 else 1
     # Send in the idc side joins in the irc side and vice-versa
-    chat.send(
-        "PRIVMSG",
-        relayedChannels[whichchan],
-        "(%s) %s has joined %s"
-        % (server[which][1], hostmask[0], args[0]),
-    )
-
+    chat.send('PRIVMSG', relayedChannels[whichchan], 
+                "(%s) %s has joined %s" 
+                % (server[which][1], hostmask[0], args[0]))
 
 def relay_mode(chat, hostmask, args, which):
-    sleep(0.5)
+    #sleep(0.5)
     whichchan = 0 if which == 1 else 1
-    chat.send(
-        "PRIVMSG",
-        relayedChannels[whichchan],
-        "(%s) %s: [%s] by %s"
-        % (server[which][1], args[0], args[1], hostmask[0]),
-    )
+    chat.send('PRIVMSG', relayedChannels[whichchan], 
+                "(%s) %s: [%s] by %s" 
+                % (server[which][1], args[0], args[1], hostmask[0]))
     # Doesn't work for some reason
 
-
 def relay_quits(chat, hostmask, args, which):
-    sleep(0.5)
+    #sleep(0.5)
     whichchan = 0 if which == 1 else 1
     # Send in the idc side quits in the irc side and vice-versa
-    chat.send(
-        "PRIVMSG",
-        relayedChannels[whichchan],
-        "(%s) %s has quit %s"
-        % (server[which][1], hostmask[0], args[0]),
-    )
-
+    chat.send('PRIVMSG', relayedChannels[whichchan], 
+                "(%s) %s has quit %s" 
+                % (server[which][1], hostmask[0], args[0]))
 
 def relay_kicks(chat, hostmask, args, which):
-    sleep(0.5)
+    #sleep(0.5)
     whichchan = 0 if which == 1 else 1
     # Send in the idc side kicks in the irc side and vice-versa
-    chat.send(
-        "PRIVMSG",
-        relayedChannels[whichchan],
-        "(%s) %s was kicked from %s by %s"
-        % (server[which][1], args[1], args[0], hostmask[0]),
-    )
-
+    chat.send('PRIVMSG', relayedChannels[whichchan], 
+                "(%s) %s was kicked from %s by %s" 
+                % (server[which][1], args[1], args[0], hostmask[0]))
 
 # Functions that run the relay functions, depending on the server where the message has been sent
-@irc.Handler("NICK", colon=False)
+@irc.Handler('NICK', colon=False)
 def handle_nicks(chat, hostmask, args):
-    if chat is idc:
+    if chat == idc:
         relay_nick(irc, hostmask, args, 0)
-    elif chat is irc:
+    elif chat == irc:
         relay_nick(idc, hostmask, args, 1)
 
 
-@irc.Handler("KICK", colon=False)
+@irc.Handler('KICK', colon=False)
 def handle_kicks(chat, hostmask, args):
-    if chat is irc:
+    if chat == irc:
         relay_kicks(idc, hostmask, args, 0)
-    elif chat is idc:
+    elif chat == idc:
         relay_kicks(irc, hostmask, args, 1)
 
 
-@miniirc.Handler("JOIN", colon=False)
+@miniirc.Handler('JOIN', colon=False)
 def handle_joins(chat, hostmask, args):
-    if chat is irc:
+    if chat == irc:
         relay_joins(idc, hostmask, args, 0)
-    elif chat is idc:
+    elif chat == idc:
         relay_joins(irc, hostmask, args, 1)
 
-
-@miniirc.Handler("PART", colon=False)
+@miniirc.Handler('PART', colon=False)
 def handle_quits(chat, hostmask, args):
-    if chat is irc:
+    if chat == irc:
         relay_quits(irc, hostmask, args, 0)
-    elif chat is idc:
+    elif chat == idc:
         relay_quits(idc, hostmask, args, 1)
 
 
-@irc.Handler("PRIVMSG", colon=False)
+@miniirc.Handler('PRIVMSG', colon=False)
 def handle_privmsgs(chat, hostmask, args):
     words = args[-1].split(" ")
     w = [x.lower() for x in words]
     channel = args[0]
     command = words[0].lower()
-    relay_msgs(idc, hostmask, args, 0)
-    print("IRC->IDC")
+    if chat == irc:
+        print("IRC->IDC")
+        relay_msgs(idc, hostmask, args, 0)
+    else:
+        relay_msgs(irc, hostmask, args, 1)
+        print("IDC->IRC")
 
-
-@idc.Handler("PRIVMSG", colon=False)
-def handle_chanmsgs(chat, hostmask, args):
+@miniirc.Handler('CHANMSG', colon=False)
+def handle_privmsgs(chat, hostmask, args):
     words = args[-1].split(" ")
     w = [x.lower() for x in words]
     channel = args[0]
     command = words[0].lower()
-    print("RECEIVEDMSG")
-    relay_msgs(irc, hostmask, args, 1)
-    print("IDC->IRC")
+    if chat == irc:
+        print("IRC->IDC")
+        relay_msgs(idc, hostmask, args, 0)
+    else:
+        relay_msgs(irc, hostmask, args, 1)
+        print("IDC->IRC")
 
 
-@miniirc.Handler("MODE", colon=False)
+
+@miniirc.Handler('MODE', colon=False)
 def handle_mode(chat, hostmask, args):
-    if chat is irc:
+    if chat == irc:
         relay_mode(irc, hostmask, args, 0)
-    elif chat is idc:
+    elif chat == idc:
         relay_mode(idc, hostmask, args, 1)
+
--- clients/srirce/miniirc_idc.py
+++ clients/srirce/miniirc_idc.py
@@ -28,85 +28,63 @@ from __future__ import annotations
 from collections.abc import Iterator, Mapping, Sequence
 from typing import Optional
 import datetime, miniirc, re, traceback  # type: ignore
+assert miniirc.ver >= (1,8,1)
 
-assert miniirc.ver >= (1, 8, 1)
 
-
-_LEADING_COLON = "" if miniirc.ver[0] > 2 else ":"
-_esc_re = re.compile(r"\\(.)")
+_LEADING_COLON = '' if miniirc.ver[0] > 2 else ':'
+_esc_re = re.compile(r'\\(.)')
 
 # Backslash must be first
-_idc_escapes = {"\\": "\\", "r": "\r", "n": "\n", "t": "\t"}
+_idc_escapes = {'\\': '\\', 'r': '\r', 'n': '\n', 't': '\t'}
 
 
-def _get_idc_args(
-    command: str, kwargs: Mapping[str, Optional[str | float]]
-) -> Iterator[str]:
+def _get_idc_args(command: str, kwargs: Mapping[str, Optional[str | float]]
+                  ) -> Iterator[str]:
     yield command
     for key, value in kwargs.items():
         if value is not None:
             value = str(value)
             for escape_char, char in _idc_escapes.items():
-                value = value.replace(char, "\\" + escape_char)
-            yield f"{key.upper()}={value}"
+                value = value.replace(char, '\\' + escape_char)
+            yield f'{key.upper()}={value}'
 
 
-def _parse_join(
-    irc: IDC,
-    hostmask: tuple[str, str, str],
-    tags: Mapping[str, str],
-    args: list[str],
-) -> None:
-    users = tags.get("=idc-join-users")
+def _parse_join(irc: IDC, hostmask: tuple[str, str, str],
+                tags: Mapping[str, str], args: list[str]) -> None:
+    users = tags.get('=idc-join-users')
     if isinstance(users, str):
-        irc._dispatch(
-            "353", "", [irc.current_nick, "=", args[0], users]
-        )
-        irc._dispatch(
-            "366", "", [irc.current_nick, args[0], "End of /NAMES list"]
-        )
+        irc._dispatch('353', '', [irc.current_nick, '=', args[0], users])
+        irc._dispatch('366', '', [irc.current_nick, args[0],
+                                  'End of /NAMES list'])
 
 
 class IDC(miniirc.IRC):
     if miniirc.ver[0] >= 2:
-
-        def _dispatch(
-            self, command: str, user: str, args: list[str]
-        ) -> None:
-            self.handle_msg(
-                miniirc.IRCMessage(
-                    command,
-                    (user, "~u", f"idc/{user}")
-                    if user
-                    else ("", "", ""),
-                    {},
-                    args,
-                )
-            )
-
+        def _dispatch(self, command: str, user: str, args: list[str]) -> None:
+            self.handle_msg(miniirc.IRCMessage(
+                command,
+                (user, '~u', f'idc/{user}') if user else ('', '', ''),
+                {},
+                args,
+            ))
     else:
-
-        def _dispatch(
-            self, command: str, user: str, args: list[str]
-        ) -> None:
+        def _dispatch(self, command: str, user: str, args: list[str]) -> None:
             if args:
                 args[-1] = _LEADING_COLON + args[-1]
             self._handle(
                 command,
-                (user, "~u", f"idc/{user}") if user else ("", "", ""),
+                (user, '~u', f'idc/{user}') if user else ('', '', ''),
                 {},
                 args,
             )
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.Handler("JOIN", colon=False, ircv3=True)(_parse_join)
+        self.Handler('JOIN', colon=False, ircv3=True)(_parse_join)
 
     def _idc_message_parser_no_exc(
         self, msg: str
-    ) -> Optional[
-        tuple[str, tuple[str, str, str], dict[str, str], list[str]]
-    ]:
+    ) -> Optional[tuple[str, tuple[str, str, str], dict[str, str], list[str]]]:
         try:
             return self.idc_message_parser(msg)
         except Exception:
@@ -115,82 +93,73 @@ class IDC(miniirc.IRC):
 
     def idc_message_parser(
         self, msg: str
-    ) -> Optional[
-        tuple[str, tuple[str, str, str], dict[str, str], list[str]]
-    ]:
+    ) -> Optional[tuple[str, tuple[str, str, str], dict[str, str], list[str]]]:
         idc_cmd = None
         idc_args = {}
-        for arg in msg.split("\t"):
-            if "=" in arg:
-                key, value = arg.split("=", 1)
+        for arg in msg.split('\t'):
+            if '=' in arg:
+                key, value = arg.split('=', 1)
                 idc_args[key] = _esc_re.sub(
-                    lambda m: _idc_escapes.get(m.group(1), "\ufffd"),
-                    value,
+                    lambda m: _idc_escapes.get(m.group(1), '\ufffd'),
+                    value
                 )
             else:
                 idc_cmd = arg
 
         # Translate IDC keyword arguments into IRC positional ones
         tags = {}
-        if idc_cmd == "PRIVMSG":
-            command = "PRIVMSG"
-            args = [self.current_nick, idc_args["MESSAGE"]]
-        elif idc_cmd == "CHANMSG":
-            command = "PRIVMSG"
-            args = ["#" + idc_args["TARGET"], idc_args["MESSAGE"]]
-        elif idc_cmd == "LOGIN_GOOD":
-            command = "001"
-            args = [
-                self.current_nick,
-                f"Welcome to IDC {self.current_nick}",
-            ]
-        elif idc_cmd == "PONG":
-            command = "PONG"
-            args = [self.ip, idc_args.get("COOKIE", "")]
-        elif idc_cmd == "JOIN":
-            command = "JOIN"
-            idc_args["SOURCE"] = self.current_nick
-            args = ["#" + idc_args["CHANNEL"]]
+        if idc_cmd == 'PRIVMSG':
+            command = 'PRIVMSG'
+            args = [self.current_nick, idc_args['MESSAGE']]
+        elif idc_cmd == 'CHANMSG':
+            command = 'PRIVMSG'
+            args = ['#' + idc_args['TARGET'], idc_args['MESSAGE']]
+        elif idc_cmd == 'LOGIN_GOOD':
+            command = '001'
+            args = [self.current_nick, f'Welcome to IDC {self.current_nick}']
+        elif idc_cmd == 'PONG':
+            command = 'PONG'
+            args = [self.ip, idc_args.get('COOKIE', '')]
+        elif idc_cmd == 'JOIN':
+            command = 'JOIN'
+            idc_args['SOURCE'] = self.current_nick
+            args = ['#' + idc_args['CHANNEL']]
 
             # HACK: Add a message tag and fire other events later rather than
             # firing events from the parser function which feels worse.
             # The tag name starts with = so that it doesn't conflict with any
             # actual IRC tags.
-            tags["=idc-join-users"] = idc_args["USERS"]
+            tags['=idc-join-users'] = idc_args['USERS']
         else:
             return None
 
         # Add generic parameters
-        if "SOURCE" in idc_args:
-            user = idc_args["SOURCE"]
-            hostmask = (user, "~u", f"idc/{user}")
-            tags["account"] = user
+        if 'SOURCE' in idc_args:
+            user = idc_args['SOURCE']
+            hostmask = (user, '~u', f'idc/{user}')
+            tags['account'] = user
         else:
-            hostmask = ("", "", "")
+            hostmask = ('', '', '')
 
-        if command == "PRIVMSG":
+        if command == 'PRIVMSG':
             # If echo-message wasn't requested then don't send self messages
-            if (
-                hostmask[0] == self.current_nick
-                and "echo-message" not in self.active_caps
-            ):
+            if (hostmask[0] == self.current_nick and
+                    'echo-message' not in self.active_caps):
                 return None
 
             # Parse the message type
-            msg_type = idc_args.get("TYPE", "").upper()
-            if msg_type == "NOTICE":
-                command = "NOTICE"
-            elif msg_type == "ACTION":
-                args[1] = f"\x01ACTION {args[1]}\x01"
+            msg_type = idc_args.get('TYPE', '').upper()
+            if msg_type == 'NOTICE':
+                command = 'NOTICE'
+            elif msg_type == 'ACTION':
+                args[1] = f'\x01ACTION {args[1]}\x01'
 
-        if "TS" in idc_args:
-            dt = datetime.datetime.utcfromtimestamp(
-                float(idc_args["TS"])
-            )
-            tags["time"] = dt.isoformat() + "Z"
+        if 'TS' in idc_args:
+            dt = datetime.datetime.utcfromtimestamp(float(idc_args['TS']))
+            tags['time'] = dt.isoformat() + 'Z'
 
-        if "LABEL" in idc_args:
-            tags["label"] = idc_args["LABEL"]
+        if 'LABEL' in idc_args:
+            tags['label'] = idc_args['LABEL']
 
         if miniirc.ver[0] >= 2:
             return miniirc.IRCMessage(command, hostmask, tags, args)
@@ -201,20 +170,12 @@ class IDC(miniirc.IRC):
 
     # Send raw messages
     def idc_send(self, command: str, **kwargs: Optional[str | float]):
-        super().quote(
-            "\t".join(_get_idc_args(command, kwargs)), force=True
-        )
+        super().quote('\t'.join(_get_idc_args(command, kwargs)), force=True)
 
-    def quote(
-        self,
-        *msg: str,
-        force: Optional[bool] = None,
-        tags: Optional[Mapping[str, str | bool]] = None,
-    ) -> None:
-        cmd, _, tags2, args = miniirc.ircv3_message_parser(
-            " ".join(msg)
-        )
-        if miniirc.ver[0] < 2 and args and args[-1].startswith(":"):
+    def quote(self, *msg: str, force: Optional[bool] = None,
+              tags: Optional[Mapping[str, str | bool]] = None) -> None:
+        cmd, _, tags2, args = miniirc.ircv3_message_parser(' '.join(msg))
+        if miniirc.ver[0] < 2 and args and args[-1].startswith(':'):
             args[-1] = args[-1][1:]
         self.send(cmd, *args, force=force, tags=tags or tags2)
 
@@ -222,62 +183,49 @@ class IDC(miniirc.IRC):
         if isinstance(self.ns_identity, tuple):
             return self.ns_identity
         else:
-            return self.ns_identity.split(" ", 1)
+            return self.ns_identity.split(' ', 1)
 
     @property
     def current_nick(self) -> str:
         return self._get_idc_account()[0]
 
-    def send(
-        self,
-        cmd: str,
-        *args: str,
-        force: Optional[bool] = None,
-        tags: Optional[Mapping[str, str | bool]] = None,
-    ) -> None:
+    def send(self, cmd: str, *args: str, force: Optional[bool] = None,
+             tags: Optional[Mapping[str, str | bool]] = None) -> None:
         cmd = cmd.upper()
-        label = tags.get("label") if tags else None
-        if cmd in ("PRIVMSG", "NOTICE"):
+        label = tags.get('label') if tags else None
+        if cmd in ('PRIVMSG', 'NOTICE'):
             target = args[0]
             # TODO: Make miniirc think that SASL worked PMs to NickServ don't
             # have to be blocked.
-            if target == "NickServ":
+            if target == 'NickServ':
                 return
 
             msg = args[1]
             msg_type: Optional[str]
-            if cmd == "NOTICE":
-                msg_type = "NOTICE"
-            elif msg.startswith("\x01ACTION"):
-                msg = msg[8:].rstrip("\x01")
-                msg_type = "ACTION"
+            if cmd == 'NOTICE':
+                msg_type = 'NOTICE'
+            elif msg.startswith('\x01ACTION'):
+                msg = msg[8:].rstrip('\x01')
+                msg_type = 'ACTION'
             else:
                 msg_type = None
 
-            if target.startswith("#"):
-                idc_cmd = "CHANMSG"
+            if target.startswith('#'):
+                idc_cmd = 'CHANMSG'
                 target = target[1:]
             else:
-                idc_cmd = "PRIVMSG"
+                idc_cmd = 'PRIVMSG'
 
-            self.idc_send(
-                idc_cmd,
-                target=target,
-                type=msg_type,
-                message=msg,
-                label=label,
-            )
-        elif cmd == "PING":
-            self.idc_send("PING", cookie=args[0], label=label)
-        elif cmd == "USER":
+            self.idc_send(idc_cmd, target=target, type=msg_type, message=msg,
+                          label=label)
+        elif cmd == 'PING':
+            self.idc_send('PING', cookie=args[0], label=label)
+        elif cmd == 'USER':
             user, password = self._get_idc_account()
-            self.idc_send(
-                "LOGIN", username=user, password=password, label=label
-            )
+            self.idc_send('LOGIN', username=user, password=password,
+                          label=label)
             self.active_caps = self.ircv3_caps & {
-                "account-tag",
-                "echo-message",
-                "labeled-response",
+                'account-tag', 'echo-message', 'labeled-response',
             }
 
     # Override the message parser to change the default parser.
